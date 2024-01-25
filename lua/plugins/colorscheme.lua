71de9295a7761713eea1bb86ccb4e87419358312
boli <bipinoli90@gmail.com>
2024-01-25 14:27:31 +0100

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+}
