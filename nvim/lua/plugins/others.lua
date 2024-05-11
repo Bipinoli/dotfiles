49723aa36314123ee3c9d6b0c33363d99177026d
boli <bipinoli90@gmail.com>
2024-05-11 15:32:44 +0200

    Installed Outline plugin

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -4,4 +4,15 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+      require("outline").setup({
+        -- Your setup opts here (leave empty to use defaults)
+      })
+    end,
+  },
 }
