0b72633699f8c7deb23c40b144b56b3539338614
boli <bipinoli90@gmail.com>
2024-07-19 13:30:09 +0200

    adding debugger wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.harpoon")
+require("neodev").setup({
+  library = { plugins = { "nvim-dap-ui" }, types = true },
+})
+require("dapui").setup()
 
 -- default theme
 vim.cmd("colorscheme molokai")
