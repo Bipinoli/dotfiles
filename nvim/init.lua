045cf40eb26435851092dc14c512dde6af3c608e
boli <>
2025-05-07 14:45:16 +0200

    removed debugger DAP

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.harpoon")
-require("neodev").setup({
-  library = { plugins = { "nvim-dap-ui" }, types = true },
-})
-require("dapui").setup()
-
 -- default theme
 vim.cmd("colorscheme catppuccin-mocha")
