4abb4f22316b57b6e8fcfbefb8985623b3ff4eda
boli <>
2025-05-07 15:41:22 +0200

    removed neovim configuration -- let's use the vanilla lazyvim

--- install.sh
+++ install.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 
 function install_dev_tools() {
-	brew upgrade
+  brew upgrade
 
-	echo "installing wezterm and neovim"
-	brew install --cask wezterm
-	brew install neovim
+  echo "installing wezterm and neovim"
+  brew install --cask wezterm
 
-	echo "installing cmd tools used by telescope plugin in nvim"
-	brew install ripgrep
-	brew install fd
+  echo "installing cmd tools used by telescope plugin in nvim"
+  brew install ripgrep
+  brew install fd
 
-	echo "installing lazygit"
-	brew install lazygit
+  echo "installing lazygit"
+  brew install lazygit
 
-	echo "installing jetbrains nerd fonts"
-	brew tap homebrew/cask-fonts
-	brew install --cask font-jetbrains-mono-nerd-font
+  echo "installing jetbrains nerd fonts"
+  brew tap homebrew/cask-fonts
+  brew install --cask font-jetbrains-mono-nerd-font
 
-	echo "Finished installing dev tools!!"
+  echo "Finished installing dev tools!!"
 }
 
 function setup_neovim() {
-	echo "configuring nvim"
+  echo "configuring nvim"
 
-	# symbolic link for the config
-	# so that the future git pulls will affect the existing config
-	mkdir -p ~/.config
-	rm -rf ~/.config/nvim
-	ln -s $(pwd)/nvim ~/.config/nvim
+  # using vanilla LazyVim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  rm -rf ~/.config/nvim/.git
+
+  # # symbolic link for the config
+  # # so that the future git pulls will affect the existing config
+  # mkdir -p ~/.config
+  # rm -rf ~/.config/nvim
+  # ln -s $(pwd)/nvim ~/.config/nvim
 }
 
 function setup_wezterm() {
-	echo "configuring wezterm"
-	ls -s $(pwd)/.wezterm.lua ~/.wezterm.lua
+  echo "configuring wezterm"
+  ls -s $(pwd)/.wezterm.lua ~/.wezterm.lua
 }
 
 install_dev_tools
 setup_neovim
 setup_wezterm
+
+"Please install neovim yourself! You could install with homebrew et al."
+"Or, what I prefer is to download the prebuilt binary from the official site. Put in ~/.local directory and update the PATH in ~/.zshrc with the path to the /bin"
--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.harpoon")
--- default theme
-vim.cmd("colorscheme catppuccin-mocha")
