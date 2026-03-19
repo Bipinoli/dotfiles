a002f2c94dffb958c06a0aa1769c5150c5ccb2e6
Bipin Oli <bipin.oli@embedded-brains.de>
2026-03-19 14:20:46 +0100

    neovim config - fuzzy find

--- neovim/init.lua
+++ neovim/init.lua
@@ -48,3 +48,14 @@ vim.keymap.set('n', '<leader>l', function()
     vim.cmd('resize 30')
   end
 end, { noremap = true, silent = true })
+
+
+-- Fuzzy file finding of a git tracked files
+vim.keymap.set('n', '<leader>f', function()
+  local fzf = io.popen('fd --type f | fzf --preview "bat --style=numbers --color=always {}"'):read('*a')
+  fzf = fzf:gsub("\n","")  -- remove newline
+  if fzf ~= "" then
+    vim.cmd('edit ' .. fzf)
+  end
+end, { noremap = true, silent = true })
+
